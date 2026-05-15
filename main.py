df89feacc956ea99ace4c61aebd7c0c630743fd5
Shreyas Sahu <sahu@Shreyass-MacBook-Pro.local>
2026-05-15 11:51:25 -0700

    Added upload endpoint & pdf extraction

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import pdfplumber
 
 app = FastAPI()
 
 @app.get("/")
 def health_check():
     return {"Status" : "Healthy"}
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile):
+    
+    if file.content_type != "application/pdf":
+        return {"error" : "invalid file type"}
+    
+    with pdfplumber.open(file.file) as pdf:
+        text = ""
+        for page in pdf.pages:
+            text += page.extract_text()
+    
+    return {"#content" : text}
